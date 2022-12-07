9553be88374bd6ff86404bcd031d1efdb3cf4ca1
shseok <shseok0674@gmail.com>
2022-12-07 15:42:36 +0900

    Use SDK/Client

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('test');
+const dotenv = require('dotenv').config()
+const {Client} = require('@notionhq/client')
+
+// Initializing a client
+const notion = new Client({
+        auth: process.env.NOTION_TOKEN,
+    })
+
+;(async () => {
+    const listUsersResponse = await notion.users.list({});
+    console.log(listUsersResponse);
+})()
