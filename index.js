f4e00a1896c7aeec80e85e58c9df8086ce6d8b10
shseok <shseok0674@gmail.com>
2022-12-07 23:03:31 +0900

    Create frontend file & Set sass with webstorm

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.static('public'));
 // get > http://localhost:5000/histories
 app.get('/histories', async (req, res) => {
     const data = await getData();
---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+const $histories = document.querySelector('#histories');
+const $loading = document.querySelector('#loading');
+let loading = false;
+
+const getHistoriesFromBackend = async () => {
+    loading = true;
+    const res = await fetch('http://localhost:5000/histories');
+    const data = await res.json();
+    loading = false;
+    return data;
+}
+
+const addHistoriesToDom = async () => {
+    const histories = await getHistoriesFromBackend();
+    if(!loading){
+        $loading.innerHTML = '';
+    }
+
+    histories.forEach(history => {
+        const div = document.createElement('div');
+        div.className = 'history';
+        div.innerHTML = `
+            <h3>${history.user}</h3>
+            <h3>${history.date}</h3>
+            <h3>${history.tags}</h3>
+        `;
+        $histories.append(div);
+    });
+}
+
+addHistoriesToDom()
