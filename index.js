0c7f9d9099402e85997926b22787b1eaff19d335
shseok <shseok0674@gmail.com>
2022-12-07 18:38:22 +0900

    Create endpoint with express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const getData = require('./services/notion');
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-;(async () => {
-    const nData = await getData();
-    console.log(nData);
-})()
+// get > http://localhost:5000/histories
+app.get('/histories', async (req, res) => {
+    const data = await getData();
+    res.json(data);
+})
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
