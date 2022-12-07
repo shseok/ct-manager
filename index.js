f618e8ec3eb7304543fc83b2afbb86e8ae8b5b07
shseok <shseok0674@gmail.com>
2022-12-07 18:31:30 +0900

    Construct data object

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const dotenv = require('dotenv').config()
-const {Client} = require('@notionhq/client')
+const getData = require('./services/notion');
 
-// Initializing a client
-const notion = new Client({
-        auth: process.env.NOTION_TOKEN,
-    })
-
-const database_id = process.env.NOTION_DATABASE_ID
-
-const getData = async ()=>{
-    const payload = {
-        database_id,
-        method: 'POST',
-    }
-    const {results} = await notion.databases.query(payload);
-    console.log(results);
-}
-getData();
+;(async () => {
+    const nData = await getData();
+    console.log(nData);
+})()
