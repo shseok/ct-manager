eaa92d359fd866a63a98529c5ce037c53306ebeb
shseok <shseok0674@gmail.com>
2023-01-10 01:29:07 +0900

    fetch all notion data

--- src/services/notion.js
+++ src/services/notion.js
@@ -10,39 +10,149 @@ const database_id = process.env.NOTION_DATABASE_ID;
 export async function getData() {
   const payload = {
     database_id,
-    // sorts: [ // 데이터가 중간에 비어있으면 정렬이 안되는 현상
-    //   {
-    //     property: 'Date',
-    //     direction: 'ascending',
-    //   },
-    // ],
+    filter: {
+      timestamp: 'created_time',
+      created_time: {
+        past_month: {},
+      },
+    },
+    sorts: [
+      // 데이터가 중간에 비어있으면 정렬이 안되는 현상
+      {
+        timestamp: 'created_time',
+        direction: 'ascending',
+      },
+    ],
   };
-  const { results } = await notion.databases.query(payload);
-  console.log(results);
-  const dict = {};
-  results.forEach((page) => {
-    if (typeof dict[page.properties.user.created_by.name] === 'undefined') {
-      dict[page.properties.user.created_by.name] = [];
-    }
-    const filterdUserObj = {
-      id: page.id,
-      user: page.properties.user.created_by.name,
-      tags:
-        page.properties.Tags.multi_select.length === 0
-          ? 'not found'
-          : page.properties.Tags.multi_select[0].name,
-      date: page.properties.Date.date?.start,
-    };
-    dict[page.properties.user.created_by.name].push(filterdUserObj);
-  });
-  for (let key in dict) {
-    // 단, 유저가 노션에서 date를 모두 입력할 수 있어야한다.
-    dict[key].sort(
-      (a, b) =>
-        parseInt(a['date'].split('-').join('')) -
-        parseInt(b['date'].split('-').join(''))
-    );
+
+  const secpayload = {
+    database_id,
+    filter: {
+      and: [
+        {
+          timestamp: 'created_time',
+          created_time: {
+            // past_year: {},
+            after: '2022-11-09',
+          },
+        },
+        {
+          timestamp: 'created_time',
+          created_time: {
+            // past_year: {},
+            before: '2022-12-09',
+          },
+        },
+      ],
+    },
+    sorts: [
+      // 데이터가 중간에 비어있으면 정렬이 안되는 현상
+      {
+        timestamp: 'created_time',
+        direction: 'ascending',
+      },
+    ],
+  };
+  const thirdpayload = {
+    database_id,
+    filter: {
+      and: [
+        {
+          timestamp: 'created_time',
+          created_time: {
+            // past_year: {},
+            after: '2022-10-09',
+          },
+        },
+        {
+          timestamp: 'created_time',
+          created_time: {
+            // past_year: {},
+            before: '2022-11-10',
+          },
+        },
+      ],
+    },
+    sorts: [
+      // 데이터가 중간에 비어있으면 정렬이 안되는 현상
+      {
+        timestamp: 'created_time',
+        direction: 'ascending',
+      },
+    ],
+  };
+  let dict;
+  let dict2;
+  let dict3;
+  try {
+    const { results } = await notion.databases.query(payload); //
+    console.log(typeof results);
+    dict = {};
+    results.forEach((page) => {
+      if (typeof dict[page.properties.user.created_by.name] === 'undefined') {
+        dict[page.properties.user.created_by.name] = [];
+      }
+      const filterdUserObj = {
+        id: page.id,
+        user: page.properties.user.created_by.name,
+        tags:
+          page.properties.Tags.multi_select.length === 0
+            ? 'not found'
+            : page.properties.Tags.multi_select[0].name,
+        date: page.properties.Date.date?.start,
+      };
+      dict[page.properties.user.created_by.name].push(filterdUserObj);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  console.log(dict);
+
+  try {
+    const { results } = await notion.databases.query(secpayload);
+    console.log(typeof results);
+    dict2 = {};
+    results.forEach((page) => {
+      if (typeof dict2[page.properties.user.created_by.name] === 'undefined') {
+        dict2[page.properties.user.created_by.name] = [];
+      }
+      const filterdUserObj = {
+        id: page.id,
+        user: page.properties.user.created_by.name,
+        tags:
+          page.properties.Tags.multi_select.length === 0
+            ? 'not found'
+            : page.properties.Tags.multi_select[0].name,
+        date: page.properties.Date.date?.start,
+      };
+      dict2[page.properties.user.created_by.name].push(filterdUserObj);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const { results } = await notion.databases.query(thirdpayload);
+    console.log(typeof results);
+    dict3 = {};
+    results.forEach((page) => {
+      if (typeof dict3[page.properties.user.created_by.name] === 'undefined') {
+        dict3[page.properties.user.created_by.name] = [];
+      }
+      const filterdUserObj = {
+        id: page.id,
+        user: page.properties.user.created_by.name,
+        tags:
+          page.properties.Tags.multi_select.length === 0
+            ? 'not found'
+            : page.properties.Tags.multi_select[0].name,
+        date: page.properties.Date.date?.start,
+      };
+      dict3[page.properties.user.created_by.name].push(filterdUserObj);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  const dict4 = dict3['현석 신'].concat(dict2['현석 신']);
+  console.log(dict4.concat(dict['현석 신']));
   return dict;
 }
